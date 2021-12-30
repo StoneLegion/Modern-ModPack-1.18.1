4cedbaa8d07fc1f5019f2ba0805c873c1d656489
Kane Hart <kanechart@gmail.com>
2021-12-30 01:11:19 -0500

    Updated Client Script to remove "useless" crimson tools.

--- client/kubejs/client_scripts/script.js
+++ client/kubejs/client_scripts/script.js
@@ -2,7 +2,25 @@
 
 console.info('Hello, World! (You will see this line every time client resources reload)')
 
-onEvent('jei.hide.items', event => {
+onEvent('rei.hide.items', event => {
 	// Hide items in JEI here
-	// event.hide('minecraft:cobblestone')
+	event.hide('crimsonshammers:hammer_wooden')
+	event.hide('crimsonshammers:hammer_stone')
+	event.hide('crimsonshammers:hammer_golden')
+	event.hide('crimsonshammers:hammer_copper')
+
+	event.hide('crimsonshammers:excavator_wooden')
+	event.hide('crimsonshammers:excavator_stone')
+	event.hide('crimsonshammers:excavator_golden')
+	event.hide('crimsonshammers:excavator_copper')
+
+	event.hide('crimsonshammers:lumber_wooden')
+	event.hide('crimsonshammers:lumber_stone')
+	event.hide('crimsonshammers:lumber_golden')
+	event.hide('crimsonshammers:lumber_copper')
+
+	event.hide('crimsonshammers:handle_wooden')
+	event.hide('crimsonshammers:handle_stone')
+	event.hide('crimsonshammers:handle_golden')
+	event.hide('crimsonshammers:handle_copper')
 })
